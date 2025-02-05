11cf280387e3458deb0b1f7ff0b1747bff976ec3
Tobias Butler <tobiasab@gmail.com>
2025-02-04 23:22:39 -0800

    set up expo-shazamkit demo

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import * as ExpoShazamKit from 'expo-shazamkit';
+import { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+
 
 export default function App() {
+  const [expoShazamResult, setExpoShazamResult] = useState(null);
+  const [expoShazamError, setExpoShazamError] = useState(null);
+  const [listening, setListening] = useState(false);
+
+  useEffect(() => {
+    // TODO: We will need to set a ShazamKit developer token on Android
+    // but the API currently does not exist in the plugin
+    if (Platform.OS === 'android') {
+      // ExpoShazamKit.setDeveloperToken('DEVELOPER_TOKEN_HERE');
+    }
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title={listening ? 'Running Shazam' : 'Run Shazam'} onPress={async () => {
+        setListening(true);
+        setExpoShazamResult(null);
+        setExpoShazamError(null);
+        try {
+          const shazamResult = await ExpoShazamKit.startListening();
+          setExpoShazamResult(shazamResult);
+        } catch (e) {
+          setExpoShazamError(e);
+        } finally {
+          setListening(false);
+        }
+      }} />
+      {listening && <Text>Listening...</Text>}
+      {expoShazamResult && <Text style={styles.pre}>{JSON.stringify(expoShazamResult, null, 2)}</Text>}
+      {expoShazamError && <Text style={[styles.pre, { color: 'red' }]}>{expoShazamError.message}</Text>}
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +44,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  pre: {
+    whiteSpace: 'pre',
+    fontFamily: 'monospace',
   },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+Expo Shazamkit Demo
+
+Works on iOS
+
+Install dependencies using `yarn install`
+
+Tested by building on EAS (`eas build --profile development --platform ios`)
